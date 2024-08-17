536c442a6434ccc5c491b0337d424937a074218a
Mykhailo Tokmakov <misha.tokmak@gmail.com>
2024-08-17 10:10:53 +0300

    Initial Commit

---
+++ SportNews/SportNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportNewsApp.swift
+//  SportNews
+//
+//  Created by Michael on 8/17/24.
+//
+
+import SwiftUI
+
+@main
+struct SportNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
