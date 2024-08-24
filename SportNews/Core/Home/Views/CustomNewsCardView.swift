2e188d44ba4395358083eb76348e6edfb4e3704b
Mykhailo Tokmakov <misha.tokmak@gmail.com>
2024-08-24 17:10:42 +0300

    Create newsRowView, create logic of settingView
    Add logic to shareButton, add customNewsRowView rotation package, add notificationView to setting list

---
+++ SportNews/Core/Home/Views/CustomNewsCardView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomNewsCardView.swift
+//  SportNews
+//
+//  Created by Michael on 8/23/24.
+//
+
+import SwiftUI
+
+struct CustomNewsCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomNewsCardView()
+}
