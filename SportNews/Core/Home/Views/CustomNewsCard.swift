e9c2e016d73cdcb023ff18e704dcc4711acdec9f
Mykhailo Tokmakov <misha.tokmak@gmail.com>
2024-11-07 19:40:32 +0200

    Sorting MySavedView, NewsCard size, customizations
    Sorting news based on categories for MySavedView, customize buttons, replace image for not loaded image's in API, Create custom size for NewsCard on any device.

---
+++ SportNews/Core/Home/Views/CustomNewsCard.swift
@@ -0,0 +1,18 @@
+//
+//  CustomNewsCard.swift
+//  SportNews
+//
+//  Created by Michael on 11/7/24.
+//
+
+import SwiftUI
+
+struct CustomNewsCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomNewsCard()
+}
